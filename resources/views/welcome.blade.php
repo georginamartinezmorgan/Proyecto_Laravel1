0d673b80d102dac4b6ec8065b848d96bc3d3ffe1
JulissaCorrales <julissa.corrales@unah.hn>
2020-09-23 18:49:03 -0600

    cambio juli

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel probando
                 </div>
 
                 <div class="links">
