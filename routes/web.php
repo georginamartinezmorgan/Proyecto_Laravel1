e1224e87f99f1562dcb3518746ec07c73a3490eb
junioranibalsalinasmartinez <junior.salinas@unah.hn>
2020-09-23 19:31:16 -0600

    Juniorhola

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('litros',function(){
-    return "Buenos ";
+    return "Buenos dias y noches";
 });
 
 Route ::get('/autos','AutoController@indice');
